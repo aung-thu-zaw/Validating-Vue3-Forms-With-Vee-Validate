87333ab65768868ee55457cd4ab4f00a9b5bb6dc
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2023-09-26 19:52:23 +0630

    project setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    class="container mx-auto min-h-screen flex flex-col items-center justify-center"
+  >
+    <h1 class="text-3xl text-slate-600 font-bold mb-5">This is Home Page</h1>
+    <div class="flex items-center space-x-5">
+      <router-link
+        :to="{ name: 'register' }"
+        class="bg-blue-600 text-white text-xs rounded-sm px-3 py-2 font-semibold hover:bg-blue-700"
+      >
+        Register
+      </router-link>
+      <router-link
+        :to="{ name: 'login' }"
+        class="bg-gray-100 text-slate-700 text-xs rounded-sm px-3 py-2 font-semibold hover:bg-gray-200 border border-gray-300"
+      >
+        Login
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
